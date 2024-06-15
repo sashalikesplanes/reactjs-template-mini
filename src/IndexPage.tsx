42c38ef4e8b665986ae61fd7dee8c91b1011668a
Sasha Kiselev <sdkiselev1812+github@gmail.com>
2024-06-15 13:30:15 +0300

    further simplify

--- src/IndexPage.tsx
+++ src/IndexPage.tsx
@@ -1,24 +1,11 @@
-import { Section, Cell, Image, List } from "@telegram-apps/telegram-ui";
+import { Section, Cell, List } from "@telegram-apps/telegram-ui";
 import type { FC } from "react";
-
-import tonSvg from "./ton.svg";
 
 import "./IndexPage.css";
 
 export const IndexPage: FC = () => {
   return (
     <List>
-      <Section
-        header="Features"
-        footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-      >
-        <Cell
-          before={<Image src={tonSvg} style={{ backgroundColor: "#007AFF" }} />}
-          subtitle="Connect your TON wallet"
-        >
-          TON Connect
-        </Cell>
-      </Section>
       <Section
         header="Application Launch Data"
         footer="These pages help developer to learn more about current launch information"
